b0d850fedc57c3ec70d921a288080ccb35bb7c87
Mariley20 <mariley2018@gmail.com>
2017-09-20 17:12:56 -0500

    procedimental

---
+++ assests/js/solucion_procedimental.js
@@ -0,0 +1,69 @@
+let nombre;
+let puntajeTecnico;
+let poncentajeHSE;
+let limpiar = true;
+let estudiante = [
+    { nombre: "mariley", porcentajTecnico: "78", hse: "78" },
+    { nombre: "yudit", porcentajTecnico: "90", hse: "90" },
+    { nombre: "ka;ina", porcentajTecnico: "23", hse: "89" }
+];
+inicio() => {
+        this.nombre = $('#inputNombreApellido');
+        this.puntajeTecnico = $('#inputPorcentajeTecnico');
+        this.poncentajeHSE = $('#inputHSE');
+        recordsEstudiante.configuracionBTN();
+    },
+    configuracionBTN() => {
+        $('#agregarEstudiante').click(recordsEstudiante.guardarDatoEstudiante);
+        $('#mostrarLista').click(recordsEstudiante.mostrarListaEstudiantes);
+    },
+    guardarDatoEstudiante() => {
+        recordsEstudiante.limpiarAlerta();
+        if (this.nombre.val() == "" || this.puntajeTecnico.val() == "" || this.poncentajeHSE.val() == "") {
+            $('#alerta').append(`<div class="alert alert-danger" role="alert">\
+            !Escribe algo.</div>`);
+        } else {
+            let datos = {
+                nombre: this.nombre.val(),
+                porcentajTecnico: this.puntajeTecnico.val(),
+                hse: this.poncentajeHSE.val()
+            }
+            recordsEstudiante.estudiante.push(datos);
+            $('#alerta').append(`<div class="alert alert-success" role="alert">\
+                 !Registrado con Exito.</div>`);
+            recordsEstudiante.limpiarFormulario();
+            recordsEstudiante.imprimirUltimo();
+            console.log(recordsEstudiante.estudiante);
+        }
+    },
+    limpiarFormulario: () => {
+        $('#inputNombreApellido').val("");
+        $('#inputPorcentajeTecnico').val("");
+        $('#inputHSE').val("")
+    },
+    imprimirUltimo: () => {
+        let indice = recordsEstudiante.estudiante.length - 1;
+        recordsEstudiante.resultadosRecordLimpiar();
+        $('#resultadosRecord').append(`<div class="row">\
+            <div class="col-xl-4">${recordsEstudiante.estudiante[indice].nombre}</div>\
+            <div class="col-xl-4">${recordsEstudiante.estudiante[indice].porcentajTecnico}</div>\
+            <div class="col-xl-4">${recordsEstudiante.estudiante[indice].hse}</div>\
+        </div>`);
+    },
+    resultadosRecordLimpiar: () => {
+        $('#resultadosRecord').empty();
+    },
+    limpiarAlerta: () => {
+        $('#alerta').empty();
+    },
+    mostrarListaEstudiantes: () => {
+        recordsEstudiante.estudiante.map((elemento) => {
+            $('#resultadosRecord').append(`<div class="row">\
+            <div class="col-xl-4">${elemento.nombre}</div>\
+            <div class="col-xl-4">${elemento.porcentajTecnico}</div>\
+            <div class="col-xl-4">${elemento.hse}</div>\
+        </div>`);
+        });
+    }
+
+inicio()
